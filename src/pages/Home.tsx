60d425abc407f9f91abca84e8d8b242269421399
efdalyalcin <efdalyalcin@hotmail.com>
2024-02-04 19:07:23 +0100

    feat: footer from excisting project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import ErrorPage from '@/components/ErrorCover';
+import Footer from '@/components/Footer';
 import HubItem from '@/components/HubItem';
 import Loading from '@/components/LoadingCover';
 import SearchBar from '@/components/SearchBar';
 import { getHubs } from '@/services/getHubs';
+import useSearchQueryStore from '@/store/useSearchQueryStore';
+import { useMemo } from 'react';
 import { useQuery } from 'react-query';
 
 export default function Home() {
+  const { searchQuery } = useSearchQueryStore();
+
   // this query could have been in a custom hook,
   // however for this project, it will be used for only one time so it won't make sense
   const { isError, isLoading, data, error } = useQuery({
@@ -13,17 +18,31 @@ export default function Home() {
     queryFn: getHubs,
   });
 
+  // this is a heavy operation so useMemo will be useful here
+  // additionally if I were to call API with the query Debouncing would be important
+  // but since the data is already fetched I didn't use debounce for search query
+  const filteredData = useMemo(() => {
+    return data?.filter((hub) =>
+      hub.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [data, searchQuery]);
+
   if (isError) return <ErrorPage error={error} />;
   if (isLoading) return <Loading />;
 
   return (
-    <main className="mx-auto p-4 max-w-7xl min-w-80 h-svh">
-      <SearchBar />
-      <section className="flex flex-col gap-2 pb-8">
-        {data?.map((hubData) => (
-          <HubItem hubData={hubData} key={hubData.uuid} />
-        ))}
-      </section>
-    </main>
+    <>
+      <main className="mx-auto p-4 max-w-7xl min-w-80 h-svh">
+        <SearchBar />
+        <section className="flex flex-col gap-2">
+          {filteredData
+            ? filteredData?.map((hubData) => (
+                <HubItem hubData={hubData} key={hubData.uuid} />
+              ))
+            : null}
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ export default {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px',
     },
   },
   plugins: [],
